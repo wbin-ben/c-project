d13772b7bab213a75fab573ea04560b8d20fa561
wbin-ben <wbin_ben@163.com>
2015-06-08 20:33:36 +0800

    2.8

---
+++ chapterTwo/2.8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string str1("We are here!");
+	string str2 = "Where are you?";
+	cout << str1[0] << " , " << str1[11] << " , " << str1 << endl;
+	cout << str2[0] << " , " << str2[13] << " , " << str2 << endl;
+
+	cout << "Please input a word:";
+	cin >> str1;
+	cout << "length of the " << str1 << " is " << str1.size() <<"." << endl;
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
