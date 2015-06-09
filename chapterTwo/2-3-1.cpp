da5f1769d08b1f29e8ea26c70fd8ee7a94b46fdf
wbin-ben <wbin_ben@163.com>
2015-06-09 19:14:15 +0800

    2-3-1

--- chapterTwo/2-3-1.cpp
+++ chapterTwo/2-3-1.cpp
@@ -6,13 +6,24 @@ int main()
 	string str1 = "Where are you?", str2("We are here!");
 	string str = str1 +  " " + str2;
 	cout << str << endl;
-	char s1[] = {"Where are you!"};
-	char s2[] = {"We are here!"};
-	string str3[25]={""};
-	int i, k ;
-	for(int i = 0, k = 0; str1[i] != '\0'; i++ ,k++)
-		str3[k] = str1[i];
-	for(int i = 0; str2 [i]!= '\0'; i++, k++)
-		str3[k] = str2[i];
-	cout << str3 <<endl;
+	
+	char c1[] = {"c++"},c2[] = {"program"};
+	char c3[20]={""};
+	int i,k;
+
+	for(i=0,k=0;c1[i]!='\0';i++,k++)
+		c3[k]=c1[i];
+
+	for(i=0;c2[i]!='\0';i++,k++)
+		c3[k]=c2[i];
+	cout<< c3<< endl;
+
+	char s1[] = {"Where"},s2[] = {"here!"};
+	char s3[10] = {""};
+	for(int i = 0, k = 0; s1[i] != '\0'; i++, k++)
+		s3[k] = s1[i];
+
+	for(int i = 0; s2[i] != '\0'; i ++, k ++)
+		s3[k] = s2[i];
+	cout << s3 << endl;
 }
