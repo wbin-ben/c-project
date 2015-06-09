9633d168824733dc161819a3d7677cbc3e6e37eb
wbin-ben <wbin_ben@163.com>
2015-06-09 15:58:17 +0800

    3

---
+++ chapterThree/3.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void swap(string *, string *);
+int main()
+{
+	string str1("现在"), str2("过去");
+	swap(&str1, &str2);
+	cout << "返回后: str1 = " << str1 << " str2 = " << str2 << endl;
+}
+void swap(string *s1, string *s2){
+	string temp = *s1; *s1 = *s2; *s2 = temp;
+	cout << "交换为: str1 = " << *s1 << " str2 = " << *s2 << endl;
+}
